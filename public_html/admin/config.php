ca05e5c57c5c7d6df9b6e401f1ba81364a412085
xhugox <xhugox@users.noreply.github.com>
2026-05-11 00:51:34 +0300

    Create config.php

---
+++ public_html/admin/config.php
@@ -0,0 +1,36 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'https://xn--skrynel-z8a.lt/admin/');
+define('HTTP_CATALOG', 'https://xn--skrynel-z8a.lt/');
+
+// HTTPS
+define('HTTPS_SERVER', 'https://xn--skrynel-z8a.lt/admin/');
+define('HTTPS_CATALOG', 'https://xn--skrynel-z8a.lt/');
+
+// DIR
+define('DIR_APPLICATION', '/home/admin/web/xn--skrynel-z8a.lt/public_html/admin/');
+define('DIR_SYSTEM', '/home/admin/web/xn--skrynel-z8a.lt/public_html/system/');
+define('DIR_IMAGE', '/home/admin/web/xn--skrynel-z8a.lt/public_html/image/');
+define('DIR_STORAGE', '/home/admin/web/storage/');
+define('DIR_CATALOG', '/home/admin/web/xn--skrynel-z8a.lt/public_html/catalog/');
+define('DIR_LANGUAGE', DIR_APPLICATION . 'language/');
+define('DIR_TEMPLATE', DIR_APPLICATION . 'view/template/');
+define('DIR_CONFIG', DIR_SYSTEM . 'config/');
+define('DIR_CACHE', DIR_STORAGE . 'cache/');
+define('DIR_DOWNLOAD', DIR_STORAGE . 'download/');
+define('DIR_LOGS', DIR_STORAGE . 'logs/');
+define('DIR_MODIFICATION', DIR_STORAGE . 'modification/');
+define('DIR_SESSION', DIR_STORAGE . 'session/');
+define('DIR_UPLOAD', DIR_STORAGE . 'upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'admin_skrynele');
+define('DB_PASSWORD', ';&S$FXfz409sOA+G');
+define('DB_DATABASE', 'admin_skrynele');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
+
+// OpenCart API
+define('OPENCART_SERVER', 'https://www.opencart.com/');
